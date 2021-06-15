5f5bef679b43b2a7778d71ec5486635bce97d6ed
Vanessa Ding <vanessa.ding21@gmail.com>
2021-06-15 15:00:59 -0700

    Time is displayed when Add is Clicked

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
 import Header from "./component/header"
 import './App.css';
-
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+import React from 'react';
+ 
 function App() {
-  return (
-    <div className="App">
-    <Header/>
-    </div>
-  );
+ return (
+   <div className="App">
+   <Header/>
+   <h1>
+       Enter an item into your To Do List
+   </h1>
+   <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+   <Button onClick = {clickMe}>Add</Button>
+   <script></script>
+   </div>
+ );
 }
 
-export default App;
+function clickMe()
+{
+var h=document.createElement("p");
+var t=document.createTextNode(new Date().toLocaleString());
+h.appendChild(t);
+document.body.appendChild(h);
+}
+
+ export default App;
+
