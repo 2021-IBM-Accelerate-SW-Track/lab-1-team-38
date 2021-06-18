5e03a0292312df98460d2f1c10e825d68735aecd
Aaron Gonzales <agonz243@ucsc.edu>
2021-06-18 15:20:55 -0700

    Added formatting to timestamp

--- src/todosData.js
+++ src/todosData.js
@@ -1,34 +1,35 @@
 import uuid from "react-uuid"
+import moment from "moment"
 
 const todosData = [
     {
         id: uuid(),
         text: "HackerRank Interview Prep Kit",
-        dateAndTime: new Date().toLocaleString(),
+        dateAndTime: new moment(Date()).format("MMM Mo LT"),
         completed: true
     },
     {
         id: uuid(),
         text: "“Learn DS & Algorithms” course from Programiz",
-        dateAndTime: new Date().toLocaleString(),
+        dateAndTime: new moment(Date()).format("MMM Mo LT"),
         completed: false
     },
     {
         id: uuid(),
         text: "Watch React Hooks video ",
-        dateAndTime: new Date().toLocaleString(),
+        dateAndTime: new moment(Date()).format("MMM Mo LT"),
         completed: false
     },
     {
         id: uuid(),
         text: "Watch Material UI video",
-        dateAndTime: new Date().toLocaleString(),
+        dateAndTime: new moment(Date()).format("MMM Mo LT"),
         completed: false
     },
     {
         id: uuid(),
         text: "Read React Docs",
-        dateAndTime: new Date().toLocaleString(),
+        dateAndTime: new moment(Date()).format("MMM Mo LT"),
         completed: false
     }
 ]
