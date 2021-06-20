d14e34c9849e07c81574312c76c47b5a617fbe77
Gurpreet <66855895+gudhillon@users.noreply.github.com>
2021-06-19 21:03:11 -0700

    added more attributes

--- src/todosData.js
+++ src/todosData.js
@@ -5,31 +5,41 @@ const todosData = [
         id: uuid(),
         text: "HackerRank Interview Prep Kit",
         dateAndTime: new Date().toLocaleString(),
-        completed: true
+        completed: true,
+        isEditing: false,
+        placeholder: "",
     },
     {
         id: uuid(),
         text: "“Learn DS & Algorithms” course from Programiz",
         dateAndTime: new Date().toLocaleString(),
-        completed: false
+        completed: false,
+        isEditing: false,
+        placeholder: "",
     },
     {
         id: uuid(),
         text: "Watch React Hooks video ",
         dateAndTime: new Date().toLocaleString(),
-        completed: false
+        completed: false,
+        isEditing: false,
+        placeholder: "",
     },
     {
         id: uuid(),
         text: "Watch Material UI video",
         dateAndTime: new Date().toLocaleString(),
-        completed: false
+        completed: false,
+        isEditing: false,
+        placeholder: "",
     },
     {
         id: uuid(),
         text: "Read React Docs",
         dateAndTime: new Date().toLocaleString(),
-        completed: false
+        completed: false,
+        isEditing: false,
+        placeholder: "",
     }
 ]
 
